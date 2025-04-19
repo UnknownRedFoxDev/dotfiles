030dc2ae2c4154aef373199d3fe3783a1ddf5ee2
Liam B. <114443299+UnknownRedFoxDev@users.noreply.github.com>
2025-04-19 22:40:46 +0200

    initial commit

---
+++ .config/lvim/config.lua
@@ -0,0 +1,13 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.tabstop = 4       -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4    -- Number of spaces for auto-indent
+vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.smartindent = true -- Enable smart indentation
+vim.opt.autoindent = true -- Maintain indentation from previous line
+lvim.builtin.autopairs.active = false
+vim.opt.relativenumber = true
